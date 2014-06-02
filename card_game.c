356fbbf895f38461cffed5600bb5988faa41603f
Stefan Iliev <siliev@localhost.localdomain>
2014-06-02 18:14:28 +0300

    Damn csv file for special project.

--- card_game.c
+++ card_game.c
@@ -43,12 +43,35 @@ struct card_t GenerateCard() {
 	return GeneratedCard;
 }
 
-void GenerateMultipleCards(int NumberOfCards) {
+struct deck_t GenerateMultipleCards(int NumberOfCards) {
 	int i = 0;
+	struct deck_t deck;
+	deck.size = 0;
 	while (i < NumberOfCards) {
-		
+		push_card(GenerateCard(),&deck);
+		i++;
 	}
+	return deck;
 }
+
+int WriteGeneratedDeckToFile(struct deck_t *deck,char *filename) {
+	FILE *fp;
+	int prevSize = deck -> size;
+	struct card_t CardToWrite;
+	fp = fopen(filename, "w");
+	if (fp == NULL) return 0; //Check for errors in creating/rewriting file.
+	while( deck->size != 0 ) {
+		
+		draw_card(deck,&CardToWrite);
+		fprintf(fp,"NEMA IME,%d,%d,%d,\n",CardToWrite.mana,CardToWrite.damage,CardToWrite.health);
+
+	}
+	deck -> size = prevSize;
+	fclose(fp);
+	return 1;
+}
+
+//Fuck it.GG.Nabs.Bronze 5.EZ. inb4 200 errors
 
 int main()
 {
@@ -58,6 +81,8 @@ int main()
 	int pCard = 0;
 	int mLane = 0;
 	struct card_t generatedCard;
+	struct deck_t generatedDeck = GenerateMultipleCards(30);
+	WriteGeneratedDeckToFile(&generatedDeck,"idiocracy.csv");
 	generatedCard = GenerateCard();
 	print_card(generatedCard,1);
 	return 0;
